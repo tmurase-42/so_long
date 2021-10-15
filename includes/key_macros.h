16bd11a07a4f556045726e47d6c417cb34fbc9ec
tmurase-42 <darekamizuw@gmail.com>
2021-10-15 12:34:10 +0900

    textureを変更

--- includes/key_macros.h
+++ includes/key_macros.h
@@ -6,7 +6,7 @@
 /*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 17:40:13 by oouklich          #+#    #+#             */
-/*   Updated: 2021/10/14 14:36:49 by tmurase          ###   ########.fr       */
+/*   Updated: 2021/10/15 11:07:09 by tmurase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,15 @@
 #  define X_EVENT_KEY_PRESS 2
 #  define X_EVNET_KEY_RELEASE 3
 #  define X_EVENT_KEY_EXIT 33
-//#  define K_A 113
-//#  define K_D 100
-//#  define K_S 115
-//#  define K_W 122
 #  define K_ESC 65307
 #  define K_AR_L 65361
 #  define K_AR_R 65363
-# define Z 122
-# define Q 113
-# define K_S 115
-# define K_D 100
-# define K_W 119
-# define K_A 97
+#  define K_Z 122
+#  define K_Q 113
+#  define K_S 115
+#  define K_D 100
+#  define K_W 119
+#  define K_A 97
 
 # else
 
